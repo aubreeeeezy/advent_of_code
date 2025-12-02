cf37b0ee05d9b771b07bea6a78f6e13dbdba4102
Aubree Massey <Aubree.Massey@sony.com>
2025-12-02 12:19:30 -0800

    Update IDReader.py

--- 2025/2/python/IDReader.py
+++ 2025/2/python/IDReader.py
@@ -37,64 +37,24 @@ class IDReader:
     @staticmethod
     def is_valid(id:int) -> bool:
         """
-        Return all IDs that are invalid under the old rules.
+        Check whether an integer ID is valid under the new rules.
 
-        Uses is_valid_old() to check each ID stored in self.ids.
+        An ID is invalid if it can be written as one or more repetitions of a
+        smaller digit sequence. For example:
+        - 1212  (repeats "12")
+        - 777777 (repeats "7")
+        - 505050 (repeats "50")
 
-        Returns
-        -------
-        list[int]
-            A list of IDs that fail the old validation rules.
+        Otherwise, the ID is valid.
         """
         idStr = str(id)
         idLen = len( idStr)
         for segmentLen in range(1, 1+idLen//2):
             if idLen % segmentLen == 0:
-                if len(set(__class__.tokenize_string(idStr, segmentLen))) == 1: #A little heuristic here. Not sure how optimal this is. 
+                segment = idStr[:segmentLen]
+                if segment * (idLen // segmentLen) == idStr:
                     return False
         return True
-    
-    @staticmethod
-    def tokenize_string(s, n):
-        """
-        Split a string into contiguous chunks of length n and return the unique
-        chunks as a set.
-
-        Example:
-            tokenize_string("ababab", 2) -> {"ab"}
-
-        Parameters
-        ----------
-        s : str
-            The string to tokenize.
-        n : int
-            The size of each chunk.
-
-        Returns
-        -------
-        set[str]
-            A set of unique string segments of length n.
-        """
-        return set([s[i:i+n] for i in range(0, len(s), n)])
-
-    def get_invalid_ids_old(self)  -> list[int]:
-        invalid_ids: list[int] = []
-        for id in self.ids:
-            if __class__.is_valid_old(id):
-                pass
-            else: 
-                invalid_ids.append(id)
-        return invalid_ids
-    
-    def get_invalid_ids(self)  -> list[int]:
-        invalid_ids: list[int] = []
-        for id in self.ids:
-            if __class__.is_valid(id):
-                pass
-            else: 
-                invalid_ids.append(id)
-        return invalid_ids
-
 
     def __repr__(self) -> str:
         return f"{self.ids}"
@@ -122,10 +82,8 @@ if __name__ == "__main__":
         for line in f:
             for rangeStr in line.split(','):
                 reader = IDReader(rangeStr=rangeStr)
-                old_invalid_ids = reader.get_invalid_ids_old()
-                part_1_calculation += sum(old_invalid_ids)
-                invalid_ids = reader.get_invalid_ids()
-                part_2_calculation += sum(invalid_ids)
+                part_1_calculation += sum(id for id in reader.ids if not IDReader.is_valid_old(id))
+                part_2_calculation += sum(id for id in reader.ids if not IDReader.is_valid(id))
         end = time.time()
         print(f"part_1_calculation: {part_1_calculation}")
         print(f"part_2_calculation: {part_2_calculation}")
